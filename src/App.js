cbfc62f8f82d9c9de2f2b37bad052de33474a280
swc9803 <swc9803@gmail.com>
2023-07-08 02:26:46 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import Hello from "./component/Hello";
 
 function App() {
-    const trued = 'dd'
     return (
         <div className="App">
-            <h1 style={{color: 'red', opacity: 0.5}}>{trued}</h1>
             <Hello />
         </div>
     );
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,10 +1,16 @@
-import styles from "./Hello.module.css";
+import { useState } from "react";
 
 export default function Hello() {
+    const [name, setName] = useState('mike');
+
+    const changeName = () => {
+        setName(name === 'mike' ? 'Jane' : "mike")
+    }
+
     return (
         <div>
-            <p>Hello</p>
-            <div className={styles.box}>hello</div>
+            <h2>{name}</h2>
+            <button onClick={changeName}>change name</button>
         </div>
     )
 }
