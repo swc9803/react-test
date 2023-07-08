9a335037d16c0d477d694568bbe9fd19172318ca
swc9803 <swc9803@gmail.com>
2023-07-08 13:45:46 +0900

    useEffect, fetch json

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,21 +1,32 @@
-import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Word from "./Word";
 
 export default function Day() {
-  const { day } = useParams();
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+    const { day } = useParams();
+//   const wordList = words.filter(word => word.day === Number(day));
+    const [words, setWords] = useState([])
 
-  return (
-    <>
-      <h2>Day {day}</h2>
-      <table>
-        <tbody>
-          {wordList.map(word => (
-            <Word word={word} key={word.id} />
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setWords(data)
+        })
+    }, [day])
+
+    return (
+        <>
+        <h2>Day {day}</h2>
+        <table>
+            <tbody>
+            {words.map(word => (
+                <Word word={word} key={word.id} />
+            ))}
+            </tbody>
+        </table>
+        </>
+    );
 }
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
-import dummy from "../db/data.json";
+import { useState, useEffect } from "react";
 
 export default function DayList() {
+    const [days, setDays] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/days')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setDays(data)
+        })
+    }, [])
+
     return (
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key={day.id}>
                     Day {day.day}
                     <Link to={`/day/${day.day}`}>Day {day.day}</Link>
@@ -13,3 +25,4 @@ export default function DayList() {
         </ul>
     );
 }
+// json-server --watch ./src/db/data.json --port 3001
