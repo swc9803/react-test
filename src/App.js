153e44123de91a20eca9d5f82dfbe735926d530a
swc9803 <swc9803@gmail.com>
2023-07-09 00:41:10 +0900

    post 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
 import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmptyPage from './component/EmptyPage';
 
@@ -15,6 +16,7 @@ function App() {
                     <Route path="/" element={<DayList/>} />
                     <Route path="/day/:day" element={<Day/>} />
                     <Route path="/create_word" element={<CreateWord />} />
+                    <Route path="/create_day" element={<CreateDay />} />
                     <Route path="*" element={<EmptyPage/>} />
                 </Routes>
             </BrowserRouter>
--- src/component/Header.js
+++ src/component/Header.js
@@ -7,12 +7,12 @@ export default function Header() {
                 <Link to="/">토익 영단어(고급)</Link>
             </h1>
             <div className="menu">
-                <Link href="create_word" className="link">
+                <Link to="/create_word" className="link">
                     단어 추가
                 </Link>
-                <a href="#x" className="link">
+                <Link to="/create_day" className="link">
                     Day 추가
-                </a>
+                </Link>
             </div>
         </div>
     );
